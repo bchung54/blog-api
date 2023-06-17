6b6a343dc49421e9615aca804174fe8b123f91a4
bchung-desktop <brianchung54@gmail.com>
2023-06-17 09:06:52 -0700

    Add cors to app

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.set('view engine', 'ejs');
 passportConfig(passport);
 
 app.use(passport.initialize());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
